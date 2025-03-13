af743200f86a5f275d959a5797c3303d8575aa15
JeongMin Seo <seop2569@gmail.com>
2025-03-13 13:14:36 +0900

    abc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,16 @@ function Header() {
         border: "1px solid black",
         padding: 20,
         textAlign: "center",
+        display: "flex",
+        justifyContent: "space-between",
       }}
     >
-      header
+      <div>JeongMin-S</div>
+      <div>
+        <input type="text" placeholder="Search"></input>
+        <button>button</button>
+      </div>
+      <div>userIcon</div>
     </header>
   );
 }
