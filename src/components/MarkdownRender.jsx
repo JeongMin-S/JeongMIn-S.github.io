38ceccd8ce7406c2588977faab77cf1efd558ce5
JeongMin Seo <seop2569@gmail.com>
2025-03-21 11:25:26 +0900

    postTest

--- src/components/MarkdownRender.jsx
+++ src/components/MarkdownRender.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { fetchMarkdownContent } from "../util/github"; // 올바른 경로 확인
+import "github-markdown-css"; // 글로벌로 CSS를 불러옴
 
 const MarkdownRenderer = ({ filename }) => {
   const [content, setContent] = useState("");
@@ -10,7 +11,7 @@ const MarkdownRenderer = ({ filename }) => {
   }, [filename]);
 
   return (
-    <div className="prose mx-auto">
+    <div className="markdown-body" style={{ padding: "1rem" }}>
       <ReactMarkdown>{content}</ReactMarkdown>
     </div>
   );
