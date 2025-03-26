b6d01368e6e363cb58354e842b5c35d5d34b9b5e
JeongMin Seo <seop2569@gmail.com>
2025-03-27 00:30:49 +0900

    Setup GitHub Actions for automatic deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  define: {
+    "process.env": process.env, // 환경 변수를 전역으로 사용 가능하게 설정
+  },
 });
